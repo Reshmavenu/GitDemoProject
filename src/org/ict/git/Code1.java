e92dcba63913f23c873144b96ee1a7bf25cadada
reshma p <reshma p@LAPTOP-1N14IG11>
2024-01-19 13:08:09 +0530

    first commit

---
+++ src/org/ict/git/Code1.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+package org.ict.git;
+
+/**
+ * 
+ */
+public class Code1 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int num1=10;
+int num2=30;
+int S = num1+num2;
+System.out.println("the sum of the numbers is "+S);
+	}
+
+}
